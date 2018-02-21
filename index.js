2db0d380c0786dda06a7fc4d8ca16f602aebe1d6
Gabriel Nuñez <gabriel.nzn@gmail.com>
2018-02-21 23:07:54 +0000

    Creando componentes

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('HOLA :D')
+import React from 'react';
+import { render } from 'react-dom';
+import Media from './src/playlist/components/media';
+
+const app = document.getElementById('app');
+
+// reactDOM.render(Qué voy a renderizar, dónde lo haré);
+//const holaMundo = <h1>Hola mundo! :D</h1>;
+render(<Media />, app);
