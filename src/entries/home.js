0e531b3ede67d31e7f575c3d46a14341be516772
Gabriel Nuñez <gabriel.nzn@gmail.com>
2018-04-10 23:34:34 +0100

    Estructurando el proyecto con Containers y UI components

--- src/entries/home.js
+++ src/entries/home.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Playlist from './src/playlist/components/playlist';
-import data from './src/api.json';
+import Home from '../pages/containers/home';
+// import Playlist from './src/playlist/components/playlist';
+// import data from './src/api.json';
 
 const app = document.getElementById('app');
 
 // reactDOM.render(Qué voy a renderizar, dónde lo haré);
 //const holaMundo = <h1>Hola mundo! :D</h1>;
-render(<Playlist data={data} />, app);
+render(<Home />, app);
---
+++ src/pages/components/home-layout.js
@@ -0,0 +1,12 @@
+import React from 'React';
+
+function HomeLayout(props) {
+    return (
+        <section>
+            layout
+            {props.children}
+        </section>
+    )
+}
+
+export default HomeLayout;
