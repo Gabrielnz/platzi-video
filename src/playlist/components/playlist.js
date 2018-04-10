c7b652c36d270f0144c6b4374effb4a694d68fa6
Gabriel Nuñez <gabriel.nzn@gmail.com>
2018-04-10 22:51:28 +0100

    Creando íconos de pausa, volumen y pantalla completa.

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Media from './media';
 import './playlist.css';
 import Play from '../../icons/components/play';
+import Pause from '../../icons/components/pause';
+import Volume from '../../icons/components/volume';
+import FullScreen from '../../icons/components/full-screen';
 
 function Playlist(props) {
 	const playlist = props.data.categories[0].playlist;
@@ -9,6 +12,18 @@ function Playlist(props) {
 	return (
 		<div className="Playlist">
 			<Play
+				size={100}
+				color="red"
+			/>
+			<Pause
+				size={100}
+				color="red"
+			/>
+			<Volume
+				size={100}
+				color="red"
+			/>
+			<FullScreen
 				size={100}
 				color="red"
 			/>
