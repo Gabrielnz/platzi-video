185898d2ac559b5991ff08d85ce50d3b27deb90e
Gabriel Nuñez <gabriel.nzn@gmail.com>
2018-04-14 00:11:12 +0100

    Contenido relacionado y estilos

--- src/pages/components/related.js
+++ src/pages/components/related.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import logo from '../../../images/logo.png';
+import './related.css';
 
 function Related(props) {
     return (
-        <div>
-            <img src={logo} width={250} alt=""/>
+        <div className="Related">
+            <img src={logo} width={250} />
         </div>
     )
 }
