2dcea7c0065072d2a242d140063442bd447ffe51
Gabriel Nuñez <gabriel.nzn@gmail.com>
2018-05-20 21:59:49 +0100

    formateando tiempo transcurrido

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -10,6 +10,7 @@ class VideoPlayer extends Component {
     state= {
         pause: true,
         duration: 0,
+        currentTime: 0,
     }
 
     togglePlay = (event) => {
@@ -27,7 +28,13 @@ class VideoPlayer extends Component {
     handleLoadedMetadata = event => {
         this.video = event.target
         this.setState({
-            duration: this.video.duration
+            duration: this.video.duration,
+        })
+    }
+
+    handleTimeUpdate = event => {
+        this.setState({
+            currentTime: this.video.currentTime,
         })
     }
 
@@ -44,12 +51,14 @@ class VideoPlayer extends Component {
                     />
                     <Timer
                         duration={this.state.duration}
+                        currentTime={this.state.currentTime}
                     />
                 </Controls>
                 <Video
                     autoplay={this.props.autoplay}
                     pause={this.state.pause}
                     handleLoadedMetadata={this.handleLoadedMetadata}
+                    handleTimeUpdate={this.handleTimeUpdate}
                     src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
                 />
             </VideoPlayerLayout>
