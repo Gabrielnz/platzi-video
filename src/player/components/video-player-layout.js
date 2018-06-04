c482111b518ba9dadad6904d299826a5d834b4d8
Gabriel Nuñez <gabriel.nzn@gmail.com>
2018-06-04 08:05:33 +0100

    botón fullscreen

--- src/player/components/video-player-layout.js
+++ src/player/components/video-player-layout.js
@@ -2,7 +2,10 @@ import React from 'react';
 import '../components/video-player-layout.css';
 
 const VideoPlayerLayout = (props) => (
-    <div className="VideoPlayer">
+    <div
+        className="VideoPlayer"
+        ref={props.setRef}
+    >
         {props.children}
     </div>
 )
